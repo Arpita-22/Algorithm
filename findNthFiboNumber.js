a4cbd92d0bffd7fd192e0f3cc87aafa4f159653a
Arpita-22 <duttaarpita22@gmail.com>
2021-01-29 11:24:59 -0800

    find Nth fibonacci number

---
+++ findNthFiboNumber.js
@@ -0,0 +1,12 @@
+//in an iterative way
+function fibo(n){
+  let firstNum = 0
+  let secondNum = 1
+  let sum = 1
+  for(let i = 2; i < n; i++ ){
+    sum  = firstNum + secondNum
+    firstNum = secondNum
+    secondNum = sum 
+  }
+  return sum
+}
