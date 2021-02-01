f048732e948d29f7cbbdd5081308dac72d3b3882
Arpita-22 <duttaarpita22@gmail.com>
2021-02-01 14:57:00 -0800

    a more optimized approach of missing number

--- missingNumber.js
+++ missingNumber.js
@@ -10,3 +10,22 @@ var missingNumber = function(nums) {
         }
     }
 };
+
+//Optimzed approach 
+var missingNumber = function(nums) {
+    let n = nums.length
+    if(n === 0){
+        return
+    }
+    //sum of n natural numbers
+    let sumOfN = n*(n+1)/2
+    let sumOfNums = 0
+    
+    for(let i = 0; i < n; i++){
+        sumOfNums += nums[i]
+    }
+    
+    let missingNo = sumOfN - sumOfNums
+    return missingNo
+    
+};
